0a21079501591d24db3b663afcf3c4e92c458959
lm <13521087562@163.com>
2023-11-23 16:43:15 +0800

    新建项目pinia持久化

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+</script>
+
+<template>
+  <div>
+    我是APP
+    <el-button>首页</el-button>
+    <p>{{ userStore.token }}</p>
+    <el-button @click="userStore.setToken('aaaaaa')">登录</el-button>
+    <el-button @click="userStore.removeToken()">退出</el-button>
+  </div>
+</template>
+
+<style scoped></style>
